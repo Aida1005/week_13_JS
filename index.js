3f38a557e048dfc4ea177ad3dd3d46e96000d02f
Tun Putera Matin <tunputeramatin@TPM-MacBook-Air.local>
2023-03-17 22:55:29 +0800

    my first js code

---
+++ index.js
@@ -0,0 +1,47 @@
+//Практическое задание
+
+//1
+let x = 40000;
+let y = 22000;
+alert (x+y);
+
+//2
+let = 'Владивосток';
+let = 2000;
+console.log ('Владивосток' + 2000);
+
+
+//3
+let time = 128;
+likes  = 254;
+vk = 137;
+youtube = 201;
+console.log (time + likes + vk + youtube);
+
+//4
+const result = 2 ** 5;
+console.log(result);
+
+//5
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+//6
+const greetings = "Привет";
+const name = " Кот";
+console.log(greetings + "," + name + "!");
+
+//7
+time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log("Вы приехали. Добро пожаловать в Москву!");
+
+//8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + " " + 'градуса по Фаренгейту — это' + " " + celsius + " " + 'градуса по Цельсию.');
